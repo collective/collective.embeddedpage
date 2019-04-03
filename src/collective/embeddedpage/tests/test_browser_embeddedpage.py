e1995a5c20a0b5b73863b9ca5f3a9234e2de0d3b
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-04-03 10:04:15 -0300

    Add tests for script and iframe changes

--- src/collective/embeddedpage/tests/test_browser_embeddedpage.py
+++ src/collective/embeddedpage/tests/test_browser_embeddedpage.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from collective.embeddedpage.testing import COLLECTIVE_EMBEDDEDPAGE_INTEGRATION_TESTING  # noqa
+from httmock import all_requests
+from httmock import HTTMock
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.textfield.value import RichTextValue
@@ -62,7 +64,39 @@ class EmbeddedPageViewIntegrationTest(unittest.TestCase):
         embedded = output.cssselect('.embeddedpage')[0]
         self.assertEqual('https://plone.org', embedded.attrib['data-embedded'])
 
-    def test_view_data_embedded(self):
-        output = self.get_parsed_data()
-        embedded = output.cssselect('.embeddedpage')[0]
-        self.assertEqual('https://plone.org', embedded.attrib['data-embedded'])
+    def test_view_script(self):
+        @all_requests
+        def response_script(url, request):
+            return {
+                'status_code': 200,
+                'content': '''
+                    <div>
+                        <script src="https://plone/org/main.js"></script>
+                    </div>
+                ''',
+            }
+        with HTTMock(response_script):
+            output = self.get_parsed_data()
+        script = output.cssselect('.embeddedpage script')[0]
+        expected = (
+            'http://nohost/plone/epage?embeddedpage_get_resource='
+            'https://plone/org/main.js'
+        )
+        self.assertEqual(expected, script.attrib['src'])
+
+    def test_view_iframe(self):
+        @all_requests
+        def response_iframe(url, request):
+            return {
+                'status_code': 200,
+                'content': '''
+                    <div>
+                        <iframe src="main.php"></iframe>
+                    </div>
+                ''',
+            }
+        with HTTMock(response_iframe):
+            output = self.get_parsed_data()
+        iframe = output.cssselect('.embeddedpage iframe')[0]
+        expected = 'https://plone.org/main.php'
+        self.assertEqual(expected, iframe.attrib['src'])
