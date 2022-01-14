0bcbedcb3e3ac58346778f360e7bb044f23cdd88
Timo Stollenwerk <stollenwerk@kitconcept.com>
2022-01-14 21:15:44 +0100

    Drop Plone Classic support

--- src/collective/embeddedpage/interfaces.py
+++ src/collective/embeddedpage/interfaces.py
@@ -2,8 +2,6 @@
 
 from collective.embeddedpage import _
 from plone.app.textfield import RichText
-from plone.autoform import directives as form
-from z3c.form.browser.checkbox import SingleCheckBoxFieldWidget
 from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
