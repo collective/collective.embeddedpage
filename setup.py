388ba3f62341b931340ee93ce74ad25fb6898f67
Timo Stollenwerk <stollenwerk@kitconcept.com>
2024-06-03 22:31:13 +0200

    Back to development: 4.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.embeddedpage",
-    version="4.0.0",
+    version="4.0.1.dev0",
     description="Add-on to embed remote HTML pages into the Plone CMS",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
